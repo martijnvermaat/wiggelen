b1c52417f9fb454c18224e3bbed60f586f022fee
Martijn Vermaat <martijn@vermaat.name>
2012-08-14 19:16:38 +0200

    More unit tests for transforms

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -16,8 +16,6 @@ from wiggelen.transform import (forward_divided_difference,
 class TestTransform(object):
     """
     Tests for the transform module.
-
-    Todo: More tests for divided difference functions.
     """
     def test_forward_divided_difference(self):
         """
@@ -28,6 +26,42 @@ class TestTransform(object):
         walker = (('a', p, v) for p, v in orig)
         assert_equal([(p, v) for _, p, v in forward_divided_difference(walker)], expected)
 
+    def test_forward_divided_difference_gaps(self):
+        """
+        Forward divided difference with gaps in walker.
+        """
+        orig = [(1, 5), (2, 4), (3, 4), (4, 4), (6, 4), (7, 3), (10, 6)]
+        expected = [(1, -1), (2, 0), (3, 0), (4, 0), (6, -1), (7, 1)]
+        walker = (('a', p, v) for p, v in orig)
+        assert_equal([(p, v) for _, p, v in forward_divided_difference(walker)], expected)
+
+    def test_forward_divided_difference_gaps_step(self):
+        """
+        Forward divided difference with gaps in walker and step=1.
+        """
+        orig = [(1, 5), (2, 4), (3, 4), (4, 4), (6, 4), (7, 3), (10, 6)]
+        expected = [(1, -1), (2, 0), (3, 0), (6, -1)]
+        walker = (('a', p, v) for p, v in orig)
+        assert_equal([(p, v) for _, p, v in forward_divided_difference(walker, step=1)], expected)
+
+    def test_forward_divided_difference_gaps_step2(self):
+        """
+        Forward divided difference with gaps in walker and step=2.
+        """
+        orig = [(1, 5), (2, 4), (3, 4), (4, 4), (6, 4), (8, 3), (10, 6)]
+        expected = [(4, 0), (6, -0.5), (8, 1.5)]
+        walker = (('a', p, v) for p, v in orig)
+        assert_equal([(p, v) for _, p, v in forward_divided_difference(walker, step=2)], expected)
+
+    def test_forward_divided_difference_gaps_auto_step(self):
+        """
+        Forward divided difference with gaps in walker and auto_step=True.
+        """
+        orig = [(1, 5), (2, 4), (3, 4), (4, 4), (6, 4), (7, 3), (10, 6)]
+        expected = [(1, -1), (2, 0), (3, 0), (6, -1)]
+        walker = (('a', p, v) for p, v in orig)
+        assert_equal([(p, v) for _, p, v in forward_divided_difference(walker, auto_step=True)], expected)
+
     def test_backward_divided_difference(self):
         """
         Simple backward divided difference.
@@ -37,6 +71,42 @@ class TestTransform(object):
         walker = (('a', p, v) for p, v in orig)
         assert_equal([(p, v) for _, p, v in backward_divided_difference(walker)], expected)
 
+    def test_backward_divided_difference_gaps(self):
+        """
+        Backward divided difference with gaps in walker.
+        """
+        orig = [(1, 5), (2, 4), (3, 4), (4, 4), (6, 4), (7, 3), (10, 6)]
+        expected = [(2, -1), (3, 0), (4, 0), (6, 0), (7, -1), (10, 1)]
+        walker = (('a', p, v) for p, v in orig)
+        assert_equal([(p, v) for _, p, v in backward_divided_difference(walker)], expected)
+
+    def test_backward_divided_difference_gaps_step(self):
+        """
+        Backward divided difference with gaps in walker and step=1.
+        """
+        orig = [(1, 5), (2, 4), (3, 4), (4, 4), (6, 4), (7, 3), (10, 6)]
+        expected = [(2, -1), (3, 0), (4, 0), (7, -1)]
+        walker = (('a', p, v) for p, v in orig)
+        assert_equal([(p, v) for _, p, v in backward_divided_difference(walker, step=1)], expected)
+
+    def test_backward_divided_difference_gaps_step2(self):
+        """
+        Backward divided difference with gaps in walker and step=2.
+        """
+        orig = [(1, 5), (2, 4), (3, 4), (4, 4), (6, 4), (8, 3), (10, 6)]
+        expected = [(6, 0), (8, -0.5), (10, 1.5)]
+        walker = (('a', p, v) for p, v in orig)
+        assert_equal([(p, v) for _, p, v in backward_divided_difference(walker, step=2)], expected)
+
+    def test_backward_divided_difference_gaps_auto_step(self):
+        """
+        Backward divided difference with gaps in walker and auto_step=True.
+        """
+        orig = [(1, 5), (2, 4), (3, 4), (4, 4), (6, 4), (7, 3), (10, 6)]
+        expected = [(2, -1), (3, 0), (4, 0), (7, -1)]
+        walker = (('a', p, v) for p, v in orig)
+        assert_equal([(p, v) for _, p, v in backward_divided_difference(walker, auto_step=True)], expected)
+
     def test_central_divided_difference(self):
         """
         Simple central divided difference.
@@ -45,3 +115,30 @@ class TestTransform(object):
         expected = [(2, -0.5), (3, 0), (4, 0.5), (5, 0), (6, -1), (7, -1.5), (8, 1), (9, 2.5)]
         walker = (('a', p, v) for p, v in orig)
         assert_equal([(p, v) for _, p, v in central_divided_difference(walker)], expected)
+
+    def test_central_divided_difference_gaps(self):
+        """
+        Central divided difference with gaps in walker.
+        """
+        orig = [(1, 5), (2, 4), (3, 4), (4, 4), (6, 4), (7, 3), (10, 6)]
+        expected = [(2, -0.5), (3, 0)]
+        walker = (('a', p, v) for p, v in orig)
+        assert_equal([(p, v) for _, p, v in central_divided_difference(walker)], expected)
+
+    def test_central_divided_difference_gaps_step(self):
+        """
+        Central divided difference with gaps in walker and step=1.
+        """
+        orig = [(1, 5), (2, 4), (3, 4), (4, 4), (6, 4), (7, 3), (10, 6)]
+        expected = [(2, -0.5), (3, 0)]
+        walker = (('a', p, v) for p, v in orig)
+        assert_equal([(p, v) for _, p, v in central_divided_difference(walker, step=1)], expected)
+
+    def test_central_divided_difference_gaps_step2(self):
+        """
+        Central divided difference with gaps in walker and step=2.
+        """
+        orig = [(1, 5), (2, 4), (3, 4), (4, 4), (6, 4), (8, 3), (10, 6)]
+        expected = [(6, -0.25), (8, 0.5)]
+        walker = (('a', p, v) for p, v in orig)
+        assert_equal([(p, v) for _, p, v in central_divided_difference(walker, step=2)], expected)
