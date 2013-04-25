1f03af1e771a3db2e7b260a8cf511c4f81b37b73
Martijn Vermaat <martijn@vermaat.name>
2013-04-25 17:02:51 +0200

    Support for custom index fields

--- wiggelen/index.py
+++ wiggelen/index.py
@@ -18,9 +18,23 @@ is a regular file). Example of the serialization we use::
 Note that we do not impose a certain order on the lines in the index nor on
 the fields on a line.
 
-.. todo:: Add some other metrics to the index (standard deviation, min, max).
+Additional custom fields can be added to the index by providing custom field
+definitions. Such a definition is a 4-tuple with the following fields:
 
-.. todo:: Mechanism to include custom metrics in the index?
+* The name of the field.
+* A function casting a field value from `string`.
+* Initial value.
+* Reducer function taking as inputs the accumulated field value, the current
+  value, and the current span, returning a new accumulated field value.
+
+As an example, the standard `sum` field could be defined as the
+following tuple::
+
+    'sum', float, 0, lambda acc, value, span: acc + value * span
+
+In practice, choose unique names for custom fields.
+
+.. todo:: Add some other metrics to the index (standard deviation, min, max).
 
 .. moduleauthor:: Martijn Vermaat <martijn@vermaat.name>
 
@@ -57,9 +71,11 @@ class ReadError(Exception):
 
 
 # Cast index values to their respective types.
-def _cast(field, value):
+def _cast(field, value, customs=None):
+    customs = customs or []
     casters = defaultdict(lambda: str,
                           start=int, stop=int, sum=float, count=int)
+    casters.update(dict((field, caster) for field, caster, _, _ in customs))
     return casters[field](value)
 
 
@@ -112,18 +128,22 @@ def write_index(idx, track=sys.stdout):
         pass
 
 
-def read_index(track=sys.stdin):
+def read_index(track=sys.stdin, customs=None):
     """
     Try to read the index from a file.
 
     :arg track: Wiggle track the index belongs to.
     :type track: file
+    :arg customs: List of custom index field definitions.
+    :type customs: list
 
     :return: Wiggle track index, or ``None`` if the index could not be read.
     :rtype: dict(str, dict(str, _))
 
     .. todo:: Only accept if index is newer than wiggle track?
     """
+    customs = customs or []
+
     filename = _index_filename(track)
 
     if filename is None:
@@ -133,20 +153,24 @@ def read_index(track=sys.stdin):
         return _cache[filename]
 
     try:
+        idx = {}
         with open(filename) as f:
-            idx = {}
             for line in f:
                 summary = {}
                 for d in line.rstrip().split(','):
                     k, v = d.split('=')
-                    summary[k] = _cast(k, v)
+                    summary[k] = _cast(k, v, customs=customs)
                 idx[summary['region']] = summary
+        # Todo: Here we check if all the required custom fields are in the
+        #     index for `_all`, but we should really do a better check if all
+        #     required data is there.
+        if all(field in idx['_all'] for field, _, _, _ in customs):
             return idx
     except IOError:
         pass
 
 
-def index(track=sys.stdin, force=False):
+def index(track=sys.stdin, force=False, customs=None):
     """
     Return index of region positions in track.
 
@@ -154,6 +178,8 @@ def index(track=sys.stdin, force=False):
     :type track: file
     :arg force: Force creating an index if it does not yet exist.
     :type force: bool
+    :arg customs: List of custom index field definitions.
+    :type customs: list
 
     :return: Wiggle track index and index filename.
     :rtype: dict(str, dict(str, _)), str
@@ -162,7 +188,9 @@ def index(track=sys.stdin, force=False):
         already exists.
     .. todo:: Handle non-writable index, corrupt index, etc.
     """
-    idx = read_index(track)
+    customs = customs or []
+
+    idx = read_index(track, customs=customs)
 
     if idx is not None or not force:
         return idx, _index_filename(track)
@@ -173,7 +201,13 @@ def index(track=sys.stdin, force=False):
         raise ReadError('Could not index track (needs random access)')
 
     region = None
-    idx = {}
+    idx = {'_all': {'region': '_all',
+                    'start':  0,
+                    'stop':   track.tell(),
+                    'sum':    0,
+                    'count':  0}}
+    idx['_all'].update(dict((field, init) for field, _, init, _ in customs))
+
     state = create_state()
 
     while True:
@@ -190,18 +224,16 @@ def index(track=sys.stdin, force=False):
                 'stop':   track.tell(),
                 'sum':    0,
                 'count':  0}
+            idx[region].update(dict((field, init)
+                                    for field, _, init, _ in customs))
         elif line_type == LineType.DATA:
-            idx[region]['stop'] = track.tell()
-            idx[region]['sum'] += data.value * data.span
-            idx[region]['count'] += data.span
-
-    # Todo: Technically, start of _all is not 0 since we always have a track
-    # line in the file.
-    idx['_all'] = {
-        'region': '_all',
-        'start':  0,
-        'stop':   track.tell(),
-        'sum':    sum(r['sum'] for r in idx.values()),
-        'count':  sum(r['count'] for r in idx.values())}
+            for r in region, '_all':
+                idx[r]['stop'] = track.tell()
+                idx[r]['sum'] += data.value * data.span
+                idx[r]['count'] += data.span
+                idx[r].update(dict((field, reducer(idx[r][field],
+                                                   data.value,
+                                                   data.span))
+                               for field, _, _, reducer in customs))
 
     return idx, write_index(idx, track)
