a18b6cef7ff6bdc1649cbe62d162083417f7b2f0
Martijn Vermaat <martijn@vermaat.name>
2013-11-29 15:29:14 +0100

    Open development for 0.3.1

--- wiggelen/__init__.py
+++ wiggelen/__init__.py
@@ -28,7 +28,7 @@ from .wiggle import ReadError, walk, zip_, fill, write
 # [2] http://semver.org/
 
 
-__version_info__ = ('0', '3', '0')
+__version_info__ = ('0', '3', '1', 'dev')
 __date__ = '29 Nov 2013'
 
 
