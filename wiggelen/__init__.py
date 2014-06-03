baf4fa781923958d00fee4d7bf33d61a11f47e5f
Martijn Vermaat <martijn@vermaat.name>
2014-06-03 16:00:11 +0200

    Bump version to 0.4.1

--- wiggelen/__init__.py
+++ wiggelen/__init__.py
@@ -28,8 +28,8 @@ from .wiggle import ReadError, walk, zip_, fill, write
 # [2] http://semver.org/
 
 
-__version_info__ = ('0', '4', '1', 'dev')
-__date__ = '17 Feb 2014'
+__version_info__ = ('0', '4', '1')
+__date__ = '3 May 2014'
 
 
 __version__ = '.'.join(__version_info__)
